48fcd246fdfe07439feb85c8f67fa3f14e273f94
douglas silva <douglass2sillva@gmail.com>
2022-04-24 11:26:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 async function buy(){
-    await fetch('Access-Control-Allow-Origin:https://api-mercado-pago.vercel.app/creat-order', {
+    await fetch('https://api-mercado-pago.vercel.app/creat-order', {
        method:'post'
     })
     .then(res=>res)
